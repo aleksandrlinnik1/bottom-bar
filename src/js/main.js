0b87b51ac8db6ff3bb89159c8f5bbed7a921a713
aleksandrlinnik1 <aleksandrlinnik1@gmail.com>
2020-03-16 00:05:39 +0300

    add autospin select logic

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import { initSounds } from './sounds';
 import { initInfo } from './info';
 import { initTurbo } from './turbo';
 import { initSpin } from './spin';
+import { initAutospin } from './autospin';
 
 initSounds();
 initBets();
 initInfo();
 initTurbo();
 initSpin();
+initAutospin();
